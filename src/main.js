efffe67f3ef5f989e9399181ab2ffbfd8737cc37
Wern Ancheta <ancheta.wern@gmail.com>
2016-02-28 14:05:20 +0800

    use the filesharer component directly

--- src/main.js
+++ src/main.js
@@ -3,15 +3,9 @@ var ReactDOM = require('react-dom');
 var Filesharer = require('./components/filesharer.jsx');
 
 var options = {
-	peerjs_key: 'your peer cloud service key'
+	peerjs_key: 'your peerjs key'
 };
-
-var Main = React.createClass({
-  render: function () {
-    return <Filesharer opts={options} />;
-  }
-});
 
 var main = document.getElementById('main');
 
-ReactDOM.render(<Main/>, main);
+ReactDOM.render(<Filesharer opts={options} />, main);
